7acd8bada9a21567cdf6b82b6abcb6535e8165c9
mialana <96770662+mialana@users.noreply.github.com>
2023-09-26 23:49:33 -0400

    small changes

--- src/shaders/background-frag.glsl
+++ src/shaders/background-frag.glsl
@@ -8,12 +8,12 @@ uniform float u_Time;
 in vec2 fs_Pos;
 out vec4 out_Col;
 
-float impulse(float k,float x){
+float gradient(float k,float x){
 	float h = k*x;
 	return h*exp(1.f-h);
 }
 
-float transform(in vec2 pos ) 
+float easeInOut(in vec2 pos ) 
 {
 	vec2 p = pos;
 	p.x *= u_Dimensions.x/u_Dimensions.y;
@@ -26,11 +26,11 @@ float transform(in vec2 pos )
 
 	float color  = 1.0 - 1.5*radius;
 
-	color = impulse(1.,color);
+	color = gradient(1.,color);
 	return pow(max(color,0.),2.);
 }
 
 void main() {
-    vec3 color = vec3(1, 0.1, 0) * (transform(fs_Pos)*0.8);
+    vec3 color = vec3(1, 0.1, 0) * (easeInOut(fs_Pos)*0.8);
 	out_Col = vec4(color,1.);
 }
